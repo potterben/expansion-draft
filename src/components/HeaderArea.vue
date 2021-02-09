7916e406371bee70aad9a5dcfe5af9da4940a5b2
Ubuntu <michaelshin@users.noreply.github.com>
2021-02-08 22:10:56 -0500

    Updated header to use bootstrap-vue. updated title

--- src/components/HeaderArea.vue
+++ src/components/HeaderArea.vue
@@ -1,25 +1,26 @@
 <template>
-  <nav class='navbar navbar-default navbar-fixed-top'>
-		<div class='fluid-container'>
-			<div class='navbar-header'>
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class='navbar-brand hidden-mobile' href='/'>NHL Expansion Draft Optimizer</a>
-			</div>
-			<div id='navbar' class='navbar-collapse collapse'>
-				<ul class='nav navbar-nav navbar-right'>
-					<li><a class='navbar-brand visible-mobile' href='/'>NHL Expansion Draft Optimizer</a></li>
-					<li><a href='/blog'>Blog</a></li>
-					<li><a href='/what'>What</a></li>
-					<li><a href='/how'>How</a></li>
-					<li><a href='/about'>About</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<b-navbar toggleable="lg" class='navbar-default navbar-fixed-top'>
+		<b-navbar-brand href="" left>
+			NHL Expansion Draft Optimizer
+		</b-navbar-brand>
+		<b-navbar-toggle target="nav-collapse" />
+		<b-collapse id="nav-collapse" is-nav right>
+			<b-navbar-nav right>
+				<b-nav-item href="/blog">
+					Blog
+				</b-nav-item>
+				<b-nav-item href="/what">
+					What
+				</b-nav-item>
+				<b-nav-item href="/how">
+					How
+				</b-nav-item>
+				<b-nav-item href="/about">
+					About
+				</b-nav-item>
+			</b-navbar-nav>     
+		</b-collapse>
+	</b-navbar>
 </template>
 
 <script>
@@ -30,7 +31,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+/* h3 {
   margin: 40px 0 0;
 }
 ul {
@@ -43,5 +44,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
