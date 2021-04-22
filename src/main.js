7423c29e65f9e217c41bb1cfed6d5b160c451969
Michael Shin <michaelwyshin@gmail.com>
2021-04-21 22:04:16 -0400

    Add first iteration of scss files

--- src/main.js
+++ src/main.js
@@ -3,11 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import './css/theme.css'
+import './styles/_main.scss'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
