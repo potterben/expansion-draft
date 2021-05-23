42b26a2e68a525c6f5791aafa6114465e074e52e
Michael Shin <michaelwyshin@gmail.com>
2021-05-23 02:09:51 -0400

    Touch up not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,11 @@
 <template>
-  <b-container class="py-4">
-    <b-row class="py-4">
+  <b-container class="p-4">
+    <b-row class="p-4">
       <b-col rows=12 class="text-center">
         <h1>Sorry we couldn't find that page!</h1>
       </b-col>
     </b-row>
-    <b-row class="py-4">
+    <b-row class="p-4">
       <b-col rows=12 class="text-center">
         <h4>Use the navigation bar above to get to the page you want.</h4>
       </b-col>
