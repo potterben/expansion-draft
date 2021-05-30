2e3665864ec691776e048e27486386e6bd227fbd
Michael Shin <michaelshin@users.noreply.github.com>
2021-05-30 18:24:26 -0400

    Moved main.py to the root directory and updated import of db.py to ensure there is no import error
    Signed-off-by: Michael Shin <michaelshin@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from typing import List
 
 import uvicorn
-from db import seed_db
+from backend.db import seed_db
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
