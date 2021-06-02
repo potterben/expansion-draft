b23f4944ba95b554c6de11c3076e77d9044c8e32
Ben Potter <benpotter@bens-mbp.lan>
2021-06-01 21:34:17 -0300

    Clean Code

--- backend/optimize/seattle_draft.py
+++ backend/optimize/seattle_draft.py
@@ -1,29 +1,41 @@
 import logging
-from typing import List, Optional
+from typing import Optional
 
 import pulp
 
-from fastapi_backend.domain import Player, Team, TeamDraft, TeamOptimizationParameters
-from fastapi_backend.draft import TeamDraft, SeattleDraft
+from backend.domain import (
+    SeattleTeamDraft,
+    TeamDraft,
+    TeamOptimizationParameters,
+)
 
 AGE_WEIGHT = 0.3
 
+
+log = logging.getLogger(__name__)
+
+
 def optimize_seattle_selection_scenario(
+    existing_teams_draft: TeamDraft,
     params: Optional[TeamOptimizationParameters] = None,
-    existing_teams_draft: TeamDraft
 ) -> pulp.LpProblem:
+
+    """Optimizes the selection decisions for Seattle.
+    :param params: Optimization parameters for Seattle
+    :param existing_teams_draft: TeamDraft object for each existing team containing name, protections, and exposures
+    :return:
     """
-        Optimizes the selection decisions for Seattle:
-        :param params: Optimization parameters for Seattle
-        :param existing_teams_draft: TeamDraft object for each existing team containing name, protections, and exposures
-        :return:
-        """
     log.info(f"Optimizing selection decisions for Seattle")
 
     # TODO get these from params
-    alpha = 0.5  # User input weight between objectives.
-    perf_metric = "ea_rating"
-    fin_metric = "cap_hit_total_scaled"
+    if params is None:
+        alpha = 0.5  # User input weight between objectives.
+        perf_metric = "ea_rating"
+        fin_metric = "cap_hit_total_scaled"
+    else:
+        alpha = params.alpha
+        perf_metric = params.perf
+        fin_metric = "cap_hit_total_scaled"
 
     # TODO put into team models
     exposed_ids = [player.id for player in existing_teams_draft.exposed_players]
@@ -34,20 +46,28 @@ def optimize_seattle_selection_scenario(
 
     # Objective
     perf_obj = pulp.lpSum(
-        [player.metrics[perf_metric] for player in existing_teams_draft.exposed_players if select_var[player.id]]
+        [
+            player.metrics[perf_metric]
+            for player in existing_teams_draft.exposed_players
+            if select_var[player.id]
+        ]
     )
     fin_obj = pulp.lpSum(
-        [player.contract[fin_metric] for player in existing_teams_draft.exposed_players if select_var[player.id]]
+        [
+            player.contract[fin_metric]
+            for player in existing_teams_draft.exposed_players
+            if select_var[player.id]
+        ]
     )
     age_obj = pulp.lpSum(
-        [player.age for player in existing_teams_draft.exposed_players if select_var[player.id]]
+        [
+            player.age
+            for player in existing_teams_draft.exposed_players
+            if select_var[player.id]
+        ]
     )
 
-    model += (
-            (1 - alpha) * perf_obj
-            - alpha * fin_obj
-            - AGE_WEIGHT * age_obj
-    )
+    model += (1 - alpha) * perf_obj - alpha * fin_obj - AGE_WEIGHT * age_obj
 
     # Select 1 player from each existing team
     for team in existing_teams_draft.team:
@@ -58,34 +78,58 @@ def optimize_seattle_selection_scenario(
 
     # Select at least 14 F, 9 D, 3 G
     select_F_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if player.forward]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if player.forward
+        ]
     )
     model += select_F_constraint >= 14, "SelectForward"
 
     select_D_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if player.defence]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if player.defence
+        ]
     )
     model += select_D_constraint >= 9, "SelectDefence"
 
     select_G_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if player.goalie]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if player.goalie
+        ]
     )
     model += select_G_constraint == 3, "SelectGoalie"
 
     # Select at least 18 skaters, 2 G under contract next year
     select_skater_contract_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if ((player.forward or player.defence) and player.under_ct)]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if ((player.forward or player.defence) and player.under_ct)
+        ]
     )
     model += select_skater_contract_constraint >= 18, "SelectContractSkater"
 
     select_G_contract_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if (player.goalie and player.under_ct)]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if (player.goalie and player.under_ct)
+        ]
     )
     model += select_G_contract_constraint >= 2, "SelectContractGoalie"
 
     # minimum and maximum team cap hit
     select_caphit_constraint = pulp.lpSum(
-    [player.contract["cap_hit_20_21"] for player in existing_teams_draft.exposed_players if select_var[player.id]]
+        [
+            player.contract["cap_hit_20_21"]
+            for player in existing_teams_draft.exposed_players
+            if select_var[player.id]
+        ]
     )
     # TODO check 60-100% team cap
     model += select_caphit_constraint >= 50, "SelectMinCapHit"
@@ -93,17 +137,29 @@ def optimize_seattle_selection_scenario(
 
     # select at least 4 C, 4 LW, 4 RW
     select_C_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if player.center]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if player.center
+        ]
     )
     model += select_C_constraint >= 4, "SelectC"
 
     select_LW_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if player.left_wing]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if player.left_wing
+        ]
     )
     model += select_LW_constraint >= 4, "SelectLW"
 
     select_RW_constraint = pulp.lpSum(
-    [select_var[player.id] for player in existing_teams_draft.exposed_players if player.right_wing]
+        [
+            select_var[player.id]
+            for player in existing_teams_draft.exposed_players
+            if player.right_wing
+        ]
     )
     model += select_RW_constraint >= 4, "SelectRW"
 
@@ -112,13 +168,15 @@ def optimize_seattle_selection_scenario(
 
     return model
 
+
 def get_seattle_draft_decisions(
-    params: TeamOptimizationParameters,
-    existing_teams_draft: TeamDraft
+    params: TeamOptimizationParameters, existing_teams_draft: TeamDraft
 ) -> SeattleTeamDraft:
     """Returns seattle decisions under the given optimization parameters."""
 
-    def model_results_to_seattle_draft(model: pulp.LpProblem, existing_teams_draft:TeamDraft) -> SeattleDraft:
+    def model_results_to_seattle_draft(
+        model: pulp.LpProblem, existing_teams_draft: TeamDraft
+    ) -> SeattleTeamDraft:
         decision_variables = model.variablesDict()
         selected_players = []
         for player in existing_teams_draft.exposed_players:
@@ -129,8 +187,10 @@ def get_seattle_draft_decisions(
             ):
                 selected_players.append(player)
 
-        return SeattleTeamDraft('Kraken', selected_players)
+        return SeattleTeamDraft("Kraken", selected_players)
 
-    seattle_selection_model = optimize_seattle_selection_scenario(params, existing_teams_draft)
+    seattle_selection_model = optimize_seattle_selection_scenario(
+        params, existing_teams_draft
+    )
 
     return model_results_to_seattle_draft(seattle_selection_model, existing_teams_draft)
