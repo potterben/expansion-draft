f36c907f237243645619f4cf87f6ec6560587b08
Michael Shin <michaelshin@users.noreply.github.com>
2021-07-07 21:52:15 -0400

    Add underline for metric names

--- src/components/IntroModal.vue
+++ src/components/IntroModal.vue
@@ -86,19 +86,19 @@
                     <h3>Choose the player performance metric</h3>
                 </b-container>
                 <b-container class="py-2">
-                    <p><strong>Performance Metrics:</strong> These metrics measure how good a player is on-ice.</p>
+                    <p><u><strong>Performance Metrics:</strong></u>metrics measure how good a player is on-ice.</p>
                 </b-container>
                 <b-container class="py-4">
                     <b-form-select v-model="performanceMetric" :options="this.performanceMetrics"></b-form-select>
                 </b-container>
                 <b-container class="pt-4">
-                    <p><strong>EA Rating:</strong> Player ratings created by EA for NHL ‘21 based on skills and physical attributes. We use a modified version of NHL ‘21 player ratings from this Reddit post.</p>
+                    <p><u><strong>EA Rating:</strong></u> Player ratings created by EA for NHL ‘21 based on skills and physical attributes. We use a modified version of NHL ‘21 player ratings from this Reddit post.</p>
                 </b-container>
                 <b-container class="py-2">
-                    <p><strong>Point Shares:</strong> Estimate of how many points a player contributes to his team over the 2020-2021 season. One point share is equivalent to one point added in the season.</p>
+                    <p><u><strong>Point Shares:</strong></u> Estimate of how many points a player contributes to his team over the 2020-2021 season. One point share is equivalent to one point added in the season.</p>
                 </b-container>
                 <b-container class="py-2">
-                    <p><strong>Game Adjusted Point Shares:</strong> Point shares extrapolated for the 2020-2021 season to account for players who miss games (e.g., due to injury).</p>
+                    <p><u><strong>Game Adjusted Point Shares:</strong></u> Point shares extrapolated for the 2020-2021 season to account for players who miss games (e.g., due to injury).</p>
                 </b-container>
             </b-container>
             <b-container v-if="currentPage==4">
@@ -106,19 +106,19 @@
                     <h3>Choose the financial flexibility metric</h3>
                 </b-container>
                 <b-container class="py-2">
-                    <p><strong>Financial Metrics:</strong> Teams are limited on how much they can spend on their players. These metrics measure how much a player affects their team’s budget.</p>
+                    <p><u><strong>Financial Metrics:</strong></u> Teams are limited on how much they can spend on their players. These metrics measure how much a player affects their team’s budget.</p>
                 </b-container>
                 <b-container class="py-4">
                     <b-form-select v-model="financialMetric" :options="this.financialMetrics"></b-form-select>
                 </b-container>
                 <b-container class="pt-4">
-                    <p><strong>Cap Hit (20/21):</strong> A player's impact on their team’s limit for the 2020-2021 season, calculated as the average annual salary of their contract excluding any performance bonuses.</p>
+                    <p><u><strong>Cap Hit (20/21):</strong></u> A player's impact on their team’s limit for the 2020-2021 season, calculated as the average annual salary of their contract excluding any performance bonuses.</p>
                 </b-container>
                 <b-container class="py-2">
-                    <p><strong>Cap Hit (21/22):</strong>  A player's impact on their team’s limit for the 2021-2022 season, calculated as the average annual salary of their contract excluding any performance bonuses.</p>
+                    <p><u><strong>Cap Hit (21/22):</strong></u>  A player's impact on their team’s limit for the 2021-2022 season, calculated as the average annual salary of their contract excluding any performance bonuses.</p>
                 </b-container>
                 <b-container class="py-2">
-                    <p><strong>Average Annual Value:</strong> The average annual salary including any performance bonuses over a player's current contract.</p>
+                    <p><u><strong>Average Annual Value:</strong></u> The average annual salary including any performance bonuses over a player's current contract.</p>
                 </b-container>
             </b-container>
             <b-container v-if="currentPage==5">
